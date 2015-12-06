608587fa88d7b05f6c3a5d77db22acd5cfd4571e
Muneeb Shahid <muneeb.shahid@mars.uni-freiburg.de>
2015-12-06 21:21:46 +0100

    updated scripts

--- scripts/shell/image_resize.sh
+++ scripts/shell/image_resize.sh
@@ -1,5 +1,6 @@
-image_dir="../../data/train"
-resized_dir=$image_dir"/resized_images"
-for image in $image_dir/*.jpg; do
+image_dir=$PROJ_HOME/"data/train/"
+original_dir=$image_dir"/original_images"
+resized_dir=$image_dir"/processed_images"
+for image in $original_dir/*.jpg; do
 	convert $image -resize 256x256\! $resized_dir/$(basename $image)
 done	

df1e5d381596c8665abbac65a98330c7aae37f00
Muneeb Shahid <shahidm@aisgpu4.rz.ais.privat>
2015-12-12 14:15:48 +0100

    merged back to master

---
+++ network/caffenet2siamese/train.py
@@ -0,0 +1,27 @@
+import caffe
+import numpy as np
+
+niter = 200
+# losses will also be stored in the log
+train_loss = np.zeros(niter)
+scratch_train_loss = np.zeros(niter)
+
+caffe.set_device(0)
+caffe.set_mode_gpu()
+# We create a solver that fine-tunes from a previously trained network.
+#solver = caffe.SGDSolver('models/finetune_flickr_style/solver.prototxt')
+#solver.net.copy_from('models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel')
+# For reference, we also create a solver that does no finetuning.
+scratch_solver = caffe.SGDSolver('solver.prototxt')
+
+# We run the solver for niter times, and record the training loss.
+for it in range(niter):
+    #solver.step(1)  # SGD by Caffe
+    scratch_solver.step(1)
+    # store the train loss
+    #train_loss[it] = solver.net.blobs['loss'].data
+    scratch_train_loss[it] = scratch_solver.net.blobs['loss'].data
+    if it % 10 == 0:
+        #print 'iter %d, finetune_loss=%f, scratch_loss=%f' % (it, train_loss[it], scratch_train_loss[it])
+        print 'iter %d, scratch_loss=%f' % (it, scratch_train_loss[it])
+print 'done'

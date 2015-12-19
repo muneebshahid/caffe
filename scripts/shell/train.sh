7f825ec24eb3cf1937f234e2b5c0f372561a3b9d
Muneeb <muneebshahid5@gmail.com>
2015-12-19 17:04:39 +0100

    updated proto.txt and modified tarinig script

--- scripts/shell/train.sh
+++ scripts/shell/train.sh
@@ -1,2 +1,9 @@
-$CAFFE_ROOT/build/tools/caffe.bin train --solver=/home/shahidm/code/domain_adaptation/network/places_caffenet/places205CNN_solver.prototxt --weights=/home/shahidm/code/domain_adaptation/data/models/alexnet_places/places205CNN_iter_300000_upgraded.caffemodel --log_dir=/home/shahidm/code/domain_adaptation/data/logs/ -gpu all
-
+if [ "$1" = "finetune" ]
+then 
+	$CAFFE_ROOT/build/tools/caffe.bin train --solver=/home/shahidm/code/domain_adaptation/network/places_caffenet/places205CNN_solver.prototxt --weights=/home/shahidm/code/domain_adaptation/data/models/alexnet_places/places205CNN_iter_300000_upgraded.caffemodel --log_dir=/home/shahidm/code/domain_adaptation/data/logs/ -gpu all
+elif [ "$1" = "scratch" ]
+then	
+	$CAFFE_ROOT/build/tools/caffe.bin train --solver=/home/shahidm/code/domain_adaptation/network/places_caffenet/places205CNN_solver.prototxt --log_dir=/home/shahidm/code/domain_adaptation/data/logs/ -gpu all
+else
+	echo "wrong params"
+fi

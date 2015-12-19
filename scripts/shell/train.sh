21e41409300a11145b53d4ba53eb65abf719ea41
Muneeb <muneebshahid5@gmail.com>
2015-12-19 12:55:30 +0100

    updated correct paths

--- scripts/shell/train.sh
+++ scripts/shell/train.sh
@@ -1,2 +1,2 @@
-$CAFFE_ROOT/build/tools/caffe.bin train -solver=/home/shahidm/code/domain_adaptation/network/places_caffenet/places205CNN_solver.prototxt -weights=/home/shahidm/code/domain_adaptation/data/models/alexnet_places/places205CNN_iter_300000_upgraded.caffemodel -gpu all
+$CAFFE_ROOT/build/tools/caffe.bin train --solver=/home/shahidm/code/domain_adaptation/network/places_caffenet/places205CNN_solver.prototxt --weights=/home/shahidm/code/domain_adaptation/data/models/alexnet_places/places205CNN_iter_300000_upgraded.caffemodel --log_dir=/home/shahidm/code/domain_adaptation/data/logs/ -gpu all
 

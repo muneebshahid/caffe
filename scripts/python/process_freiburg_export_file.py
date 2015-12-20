f1a9a736ddbe0c077ea7c6e59dd6241332007b5a
Muneeb <muneebshahid5@gmail.com>
2015-12-20 22:35:23 +0100

    modified preprocessing scripts

--- scripts/python/process_freiburg_export_file.py
+++ scripts/python/process_freiburg_export_file.py
@@ -1,42 +1,20 @@
-import os_helper as osh
-
 summer = []
 winter = []
 
-proj_home = osh.get_env_var('PROJ_HOME')
-exported_season_match = proj_home + '/data/test/unprocessed/freiburg/exported_season_match.txt'
-source_folder = proj_home + '/data/test/unprocessed/freiburg/'
-target_folder = proj_home + '/data/test/processed/freiburg/'
-
-target_folder_winter = target_folder + 'dataset/winter/'
-target_folder_summer = target_folder + 'dataset/summer/'
-copy_files = True
-
-if copy_files:
-    if osh.path_exists(target_folder_winter):
-        osh.rm_dir(target_folder_winter)
-    if osh.path_exists(target_folder_summer):
-        osh.rm_dir(target_folder_summer)
-    osh.make_dir(target_folder_winter)
-    osh.make_dir(target_folder_summer)
-
-with open(exported_season_match, "r") as lines:
+source_folder = 'D:/Uni/Study/Project/freiburg/annotator/'
+with open(source_folder + 'exported_season_match.txt', "r") as lines:
     processed_exported_season_match = []
     for line_num, line in enumerate(lines):
         line_arr = line.split(' ')
         len_arr = len(line_arr)
         if len_arr > 3:
-            im_file = source_folder + line_arr[1]
-            destination_summer = target_folder_summer + osh.extract_name_from_path(im_file)
-            osh.copy(im_file, destination_summer)
+            instance = line_arr[1]
             i = 2
             while i + 1 < len_arr:
-                im_file = source_folder + line_arr[i + 1]
-                destination_winter = target_folder_winter + osh.extract_name_from_path(im_file)
-                osh.copy(im_file, destination_winter)
-                processed_exported_season_match.append([destination_summer, destination_winter])
+                instance += " " + line_arr[i + 1]
                 i += 2
-                if line_num % 100 == 0:
-                    print str(line_num)
-    with open(target_folder + 'processed_season_match.txt', 'w') as f:
-        f.writelines([line[0] + ' ' + line[1] + '\n' for line in processed_exported_season_match])
+            if line_num % 100 == 0:
+                print str(line_num)
+            processed_exported_season_match.append(instance)
+    with open(source_folder + 'processed_season_match.txt', 'w') as f:
+        f.writelines([line + '\n' for line in processed_exported_season_match])

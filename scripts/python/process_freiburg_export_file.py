569de7e56190b8537f1367d67af32df5dd5c4d5c
Muneeb <muneebshahid5@gmail.com>
2015-12-20 13:16:14 +0100

    changed process_freiburg to filter and copy images

--- scripts/python/process_freiburg_export_file.py
+++ scripts/python/process_freiburg_export_file.py
@@ -1,45 +1,42 @@
-import filesystem_helper as fs
+import os_helper as osh
 
 summer = []
 winter = []
 
-source_folder = 'SOURCE_FOLDER'
-target_folder = 'TARGET_FOLDER'
+proj_home = '../../../freiburg/'
+exported_season_match = proj_home + 'annotator/exported_season_match.txt'
+target_folder = proj_home + 'processed/'
 
-target_folder_winter = target_folder + '/dataset/winter/'
-target_folder_summer = target_folder + '/dataset/summer/'
+target_folder_winter = target_folder + 'winter/'
+target_folder_summer = target_folder + 'summer/'
 copy_files = True
 
 if copy_files:
-	if fs.path_exists(target_folder_winter):
-		fs.rm_dir(target_folder_winter)
-	if fs.path_exists(target_folder_summer):
-		fs.rm_dir(target_folder_summer)
+    if osh.path_exists(target_folder_winter):
+        osh.rm_dir(target_folder_winter)
+    if osh.path_exists(target_folder_summer):
+        osh.rm_dir(target_folder_summer)
+    osh.make_dir(target_folder_winter)
+    osh.make_dir(target_folder_summer)
 
-	fs.make_dir(target_folder_winter)
-	fs.make_dir(target_folder_summer)
+with open(exported_season_match, "r") as lines:
+    processed_exported_season_match = []
+    for line_num, line in enumerate(lines):
+        line_arr = line.split(' ')
+        len_arr = len(line_arr)
+        if len_arr > 3:
+            im_file = line_arr[1]
+            destination_summer = target_folder_summer + osh.extract_name_from_path(im_file)
+            osh.copy(im_file, destination_summer)
+            i = 2
+            while i + 1 < len_arr:
+                im_file = line_arr[i + 1]
+                destination_winter = target_folder_winter + osh.extract_name_from_path(im_file)
+                osh.copy(im_file, destination_winter)
+                processed_exported_season_match.append([destination_summer, destination_winter])
+                i += 2
+                if line_num % 100 == 0:
+                    print str(line_num)
+    with open(target_folder + 'processed_season_match.txt', 'w') as f:
+        f.writelines([line[0] + ' ' + line[1] + '\n' for line in processed_exported_season_match])
 
-
-
-with open(source_folder + '/' + 'exported_season_match.txt', "r") as lines:
-	for line_num, line in enumerate(lines):
-		line_arr = line.split(' ')
-		len_arr = len(line_arr)
-		if len_arr > 3:
-			im_file = line_arr[1]
-			fs.copy(im_file, target_folder_summer + fs.get_file_name(im_file))		
-			i = 2
-			while i + 1 < len_arr:
-				summer.append(line_arr[0] + ',' + line_arr[1])
-				winter.append(line_arr[i] + ',' + line_arr[i + 1])
-				im_file = line_arr[i + 1]
-				fs.copy(im_file, target_folder_winter + fs.get_file_name(im_file))
-				i = i + 2
-		if line_num % 100 == 0:
-			print str(line_num)
-
-target_files = {'winter.txt': winter, 'summer.txt': summer}
-for t_file in target_files:
-	with open(target_folder + t_file, 'w') as f:
-		for line in target_files[t_file]:
-			f.write("%s\n" % line)

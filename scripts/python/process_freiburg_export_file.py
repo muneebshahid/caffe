f3c80b4c9f44d184737b8bd97c3524ddeb2f925f
Muneeb Shahid <muneeb.shahid@mars.uni-freiburg.de>
2015-12-20 13:41:52 +0100

    adaptated process_freiburg_export for local system

--- scripts/python/process_freiburg_export_file.py
+++ scripts/python/process_freiburg_export_file.py
@@ -3,12 +3,13 @@ import os_helper as osh
 summer = []
 winter = []
 
-proj_home = '../../../freiburg/'
-exported_season_match = proj_home + 'annotator/exported_season_match.txt'
-target_folder = proj_home + 'processed/'
+proj_home = osh.get_env_var('PROJ_HOME')
+exported_season_match = proj_home + '/data/test/unprocessed/freiburg/exported_season_match.txt'
+source_folder = proj_home + '/data/test/unprocessed/freiburg/'
+target_folder = proj_home + '/data/test/processed/freiburg/'
 
-target_folder_winter = target_folder + 'winter/'
-target_folder_summer = target_folder + 'summer/'
+target_folder_winter = target_folder + 'dataset/winter/'
+target_folder_summer = target_folder + 'dataset/summer/'
 copy_files = True
 
 if copy_files:
@@ -25,12 +26,12 @@ with open(exported_season_match, "r") as lines:
         line_arr = line.split(' ')
         len_arr = len(line_arr)
         if len_arr > 3:
-            im_file = line_arr[1]
+            im_file = source_folder + line_arr[1]
             destination_summer = target_folder_summer + osh.extract_name_from_path(im_file)
             osh.copy(im_file, destination_summer)
             i = 2
             while i + 1 < len_arr:
-                im_file = line_arr[i + 1]
+                im_file = source_folder + line_arr[i + 1]
                 destination_winter = target_folder_winter + osh.extract_name_from_path(im_file)
                 osh.copy(im_file, destination_winter)
                 processed_exported_season_match.append([destination_summer, destination_winter])
@@ -39,4 +40,3 @@ with open(exported_season_match, "r") as lines:
                     print str(line_num)
     with open(target_folder + 'processed_season_match.txt', 'w') as f:
         f.writelines([line[0] + ' ' + line[1] + '\n' for line in processed_exported_season_match])
-

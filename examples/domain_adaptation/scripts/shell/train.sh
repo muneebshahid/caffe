9bcea537523be786227e44a936c1682b0747c1c2
Muneeb <muneebshahid5@gmail.com>
2015-12-24 20:44:26 +0100

    training up and running

--- examples/domain_adaptation/scripts/shell/train.sh
+++ examples/domain_adaptation/scripts/shell/train.sh
@@ -2,8 +2,10 @@ if [ "$1" = "finetune" ]
 then
 	if [ "$2" = "default" ]
 	then
-		model="/home/shahidm/code/domain_adaptation/data/models/alexnet_places/places205CNN_iter_300000_upgraded.caffemodel"
-		$CAFFE_ROOT/build/tools/caffe.bin train --solver=/home/shahidm/code/domain_adaptation/network/places_caffenet/places205CNN_solver.prototxt --weights=$model --log_dir=/home/shahidm/code/domain_adaptation/data/logs/ -gpu all 
+		model=$CAFFE_ROOT"/data/domain_adaptation_data/models/alexnet/places205CNN_iter_300000_upgraded.caffemodel"
+		solver=$CAFFE_ROOT"/examples/domain_adaptation/network/alexnet/pretrained/solver.prototxt"
+		log_dir=$CAFFE_ROOT"/data/domain_adaptation_data/logs"
+		$CAFFE_ROOT/build/tools/caffe train --solver=$solver --weights=$model --log_dir=$log_dir -gpu all 
 	elif [ "$2" = "trained" ]
 	then
 		model="/home/shahidm/code/domain_adaptation/data/models/alexnet_places/trained/snapshots_iter_1000.caffemodel"

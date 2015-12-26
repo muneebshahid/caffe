91e0eb20c1143a42bf41eea70c76732031cf92fd
Muneeb <muneebshahid5@gmail.com>
2015-12-26 23:47:58 +0100

    removed bugs

--- examples/domain_adaptation/scripts/shell/create_lmdbs_resize.sh
+++ examples/domain_adaptation/scripts/shell/create_lmdbs_resize.sh
@@ -8,29 +8,29 @@ then
 	source2=$lmdb"source2"
 	rm -r $source1
 	rm -r $source2
-	convert_imageset -resize_height 256 -resize_width 256 $images $images"source1.txt" $source1
-	convert_imageset -resize_height 256 -resize_width 256 $images $images"source2.txt" $source2
+	$convert_imageset -resize_height 256 -resize_width 256 $images $images"source1.txt" $source1
+	$convert_imageset -resize_height 256 -resize_width 256 $images $images"source2.txt" $source2
 elif [ "$1" = "target" ]
 then
 	target1=$lmdb"target1"
 	target2=$lmdb"target2"
 	rm -r $target1
 	rm -r $target2
-	convert_imageset -resize_height 256 -resize_width 256 $images $images"target.txt" $target1
-	convert_imageset -resize_height 256 -resize_width 256 $images $images"target.txt" $target2
+	$convert_imageset -resize_height 256 -resize_width 256 $images $images"target1.txt" $target1
+	$convert_imageset -resize_height 256 -resize_width 256 $images $images"target2.txt" $target2
 elif [ "$1" = "test" ]
 then
 	test1=$lmdb"test1"
 	test2=$lmdb"test2"
 	rm -r $test1
 	rm -r $test2
-	convert_imageset -resize_height 256 -resize_width 256 $images $images"test1.txt" $test1
-	convert_imageset -resize_height 256 -resize_width 256 $images $images"test2.txt" $test2
+	$convert_imageset -resize_height 256 -resize_width 256 $images $images"test1.txt" $test1
+	$convert_imageset -resize_height 256 -resize_width 256 $images $images"test2.txt" $test2
 elif [ "$1" = "mean" ]
 then
 	complete_lmdb=$lmdb"complete"
 	rm -r $complete_lmdb
-	convert_imageset -resize_height 256 -resize_width 256 $images $images"complete.txt" $complete_lmdb
+	$convert_imageset -resize_height 256 -resize_width 256 $images $images"complete.txt" $complete_lmdb
 else
 	echo "Wrong Param"
 fi

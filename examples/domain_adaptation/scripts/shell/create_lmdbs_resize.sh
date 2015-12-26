f4a7b8bcb737ca0acc97d119a539f6b110fe9982
Muneeb <muneebshahid5@gmail.com>
2015-12-26 23:15:43 +0100

    modified create_lmdb to account for source and domain data

--- examples/domain_adaptation/scripts/shell/create_lmdbs_resize.sh
+++ examples/domain_adaptation/scripts/shell/create_lmdbs_resize.sh
@@ -1,26 +1,36 @@
-if [ "$1" = "train" ]
+data=$CAFFE_ROOT"/data/domain_adaptation_data/"
+convert_imageset=$CAFFE_ROOT"/build/tools/convert_imageset"
+lmdb=$data"lmdb/"
+images=$data"images/"
+if [ "$1" = "source" ]
 then
-	lmdb_train=$PROJ_HOME"/data/lmdb/train/"
-	lmdb_train1=$lmdb_train"train1"
-	lmdb_train2=$lmdb_train"train2"
-	rm -r $lmdb_train1
-	rm -r $lmdb_train2
-	$CAFFE_ROOT"/build/tools/convert_imageset" -resize_height 256 -resize_width 256 $PROJ_HOME"/data/images/" $PROJ_HOME"/data/images/train1.txt" $lmdb_train1
-	$CAFFE_ROOT"/build/tools/convert_imageset" -resize_height 256 -resize_width 256 $PROJ_HOME"/data/images/" $PROJ_HOME"/data/images/train2.txt" $lmdb_train2
+	source1=$lmdb"source1"
+	source2=$lmdb"source2"
+	rm -r $source1
+	rm -r $source2
+	convert_imageset -resize_height 256 -resize_width 256 $images $images"source1.txt" $source1
+	convert_imageset -resize_height 256 -resize_width 256 $images $images"source2.txt" $source2
+elif [ "$1" = "target" ]
+then
+	target1=$lmdb"target1"
+	target2=$lmdb"target2"
+	rm -r $target1
+	rm -r $target2
+	convert_imageset -resize_height 256 -resize_width 256 $images $images"target.txt" $target1
+	convert_imageset -resize_height 256 -resize_width 256 $images $images"target.txt" $target2
 elif [ "$1" = "test" ]
 then
-	lmdb_test=$PROJ_HOME"/data/lmdb/test/"
-	test1_lmdb=$lmdb_test"test1"
-	test2_lmdb=$lmdb_test"test2"
-	rm -r $test1_lmdb
-	rm -r $test2_lmdb
-	$CAFFE_ROOT"/build/tools/convert_imageset" -resize_height 256 -resize_width 256 $PROJ_HOME"/data/images/" $PROJ_HOME"/data/images/test1.txt" $test1_lmdb
-	$CAFFE_ROOT"/build/tools/convert_imageset" -resize_height 256 -resize_width 256 $PROJ_HOME"/data/images/" $PROJ_HOME"/data/images/test2.txt" $test2_lmdb
+	test1=$lmdb"test1"
+	test2=$lmdb"test2"
+	rm -r $test1
+	rm -r $test2
+	convert_imageset -resize_height 256 -resize_width 256 $images $images"test1.txt" $test1
+	convert_imageset -resize_height 256 -resize_width 256 $images $images"test2.txt" $test2
 elif [ "$1" = "mean" ]
 then
-	complete_lmdb=$PROJ_HOME"/data/lmdb/complete/"
+	complete_lmdb=$lmdb"complete"
 	rm -r $complete_lmdb
-	$CAFFE_ROOT"/build/tools/convert_imageset" -resize_height 256 -resize_width 256 $PROJ_HOME"/data/images/" $PROJ_HOME"/data/images/complete.txt" $complete_lmdb
+	convert_imageset -resize_height 256 -resize_width 256 $images $images"complete.txt" $complete_lmdb
 else
 	echo "Wrong Param"
 fi

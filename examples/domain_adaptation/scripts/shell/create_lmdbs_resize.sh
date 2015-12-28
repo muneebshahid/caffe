4ecbc00aa9818d4d2b38b84bf97fdd207e94d147
Muneeb <muneebshahid5@gmail.com>
2015-12-28 02:20:52 +0100

    removed a bug

--- examples/domain_adaptation/scripts/shell/create_lmdbs_resize.sh
+++ examples/domain_adaptation/scripts/shell/create_lmdbs_resize.sh
@@ -8,7 +8,7 @@ then
 	complete_lmdb=$lmdb"complete"
 	rm -r $complete_lmdb
 	$convert_imageset -resize_height 256 -resize_width 256 $images $images"complete.txt" $complete_lmdb
-elif [ $1 = "source" || $1 = "target" || $1 = "train" || $1 = "test" ]
+elif [ $1 = "source" ] || [ $1 = "target" ] || [ $1 = "train" ] || [ $1 = "test" ]
 then
 	lmdb1=$lmdb$1"1"
 	lmdb2=$lmdb$1"2"

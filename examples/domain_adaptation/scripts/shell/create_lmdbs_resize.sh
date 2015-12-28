7ef13f4303c920bc68b45e82369b1c0bba92e562
Muneeb <muneebshahid5@gmail.com>
2015-12-28 13:43:31 +0100

    modified creating lmdbs

--- examples/domain_adaptation/scripts/shell/create_lmdbs_resize.sh
+++ examples/domain_adaptation/scripts/shell/create_lmdbs_resize.sh
@@ -1,13 +1,12 @@
-data=$CAFFE_ROOT"/data/domain_adaptation_data/"
 convert_imageset=$CAFFE_ROOT"/build/tools/convert_imageset"
-lmdb=$data"lmdb/"
-images=$data"images/"
+lmdb=$CAFFE_ROOT"/data/domain_adaptation_data/lmdb/"
+images=$CAFFE_ROOT"/../data/images/"
 
-if [ $1 = "mean" ]
+if [ $1 = "complete" ]
 then
-	complete_lmdb=$lmdb"complete"
+	complete_lmdb=$lmdb$1
 	rm -r $complete_lmdb
-	$convert_imageset -resize_height 256 -resize_width 256 $images $images"complete.txt" $complete_lmdb
+	$convert_imageset -resize_height 256 -resize_width 256 $images $images$1".txt" $complete_lmdb
 elif [ $1 = "source" || $1 = "target" || $1 = "train" || $1 = "test" ]
 then
 	lmdb1=$lmdb$1"1"

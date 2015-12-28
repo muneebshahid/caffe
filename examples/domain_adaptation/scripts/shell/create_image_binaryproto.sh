f6bc273293f7c513f513451485e7b8529de7c7ca
Muneeb <muneebshahid5@gmail.com>
2015-12-28 14:55:25 +0100

    modified image_binary

--- examples/domain_adaptation/scripts/shell/create_image_binaryproto.sh
+++ examples/domain_adaptation/scripts/shell/create_image_binaryproto.sh
@@ -1,1 +1,2 @@
-$CAFFE_ROOT/build/tools/compute_image_mean -backend lmdb $PROJ_HOME/data/lmdb/complete $PROJ_HOME/data/models/scratch/mean.binaryproto
+data=$CAFFE_ROOT"/../data/"
+$CAFFE_ROOT/build/tools/compute_image_mean $CAFFE_ROOT/data/domain_adaptation_data/lmdb/complete $data/models/alexnet/scratch/mean.binaryproto

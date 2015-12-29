8d13a3a19a05685391a78251fccd25550ec89ce2
Muneeb <muneebshahid5@gmail.com>
2015-12-29 15:50:12 +0100

    removed a naming bug

--- examples/domain_adaptation/scripts/shell/prepare_data_sets.sh
+++ examples/domain_adaptation/scripts/shell/prepare_data_sets.sh
@@ -7,17 +7,17 @@ python "$PROJ_HOME/scripts/python/create_dataset_txt.py"
 if [ "$1" = "all" ] || [ "$1" = "source_target" ] || [ "$2" = "source_target" ]
 then
 	echo "Creating source lmdbs"
-	sh "$PROJ_HOME/scripts/shell/create_lmdb_resize.sh" "source"
+	sh "$PROJ_HOME/scripts/shell/create_lmdbs_resize.sh" "source"
 	echo "Creating target lmdbs"
-	sh "$PROJ_HOME/scripts/shell/create_lmdb_resize.sh" "target"
+	sh "$PROJ_HOME/scripts/shell/create_lmdbs_resize.sh" "target"
 elif [ "$1" = "all" ] || [ "$1" = "train" ] || [ "$2" = "train" ]
 then
 	echo "Creating train lmdbs"
-	sh $PROJ_HOME"/scripts/shell/create_lmdb_resize.sh" "train"
+	sh $PROJ_HOME"/scripts/shell/create_lmdbs_resize.sh" "train"
 elif [ "$1" = "all" ] || [ "$1" = "test" ] || [ "$2" = "test" ]
 then
 	echo "Creating test lmdbs"
-	sh "$PROJ_HOME/scripts/shell/create_lmdb_resize.sh" "test"
+	sh "$PROJ_HOME/scripts/shell/create_lmdbs_resize.sh" "test"
 else
 	echo "wrong or no lmdb param provided. aborting...."
 fi

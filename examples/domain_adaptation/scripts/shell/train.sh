9e4352fb3041c91d85b23e89ccbbab3cec01353c
Muneeb <muneebshahid5@gmail.com>
2015-12-29 22:31:53 +0100

    updated files

--- examples/domain_adaptation/scripts/shell/train.sh
+++ examples/domain_adaptation/scripts/shell/train.sh
@@ -1,16 +1,16 @@
 model=$CAFFE_ROOT"/../data/models/alexnet/pretrained/places205CNN_iter_300000_upgraded.caffemodel"
 network=$CAFFE_ROOT"/examples/domain_adaptation/network/alexnet/"
 log_dir=$CAFFE_ROOT"/data/domain_adaptation_data/logs"
+snapshot="snapshots_iter_1176.solverstate"
 if [ "$1" = "finetune" ]
 then
+	solver=$network"pretrained/solver.prototxt"
 	if [ "$2" = "default" ]
 	then
-		solver=$network"pretrained/solver.prototxt"
 		$CAFFE_ROOT/build/tools/caffe train --solver=$solver --weights=$model --log_dir=$log_dir -gpu 0
 	elif [ "$2" = "trained" ]
 	then
-		#snapshot="/home/shahidm/code/domain_adaptation/data/models/alexnet_places/trained/snapshots_iter_1000.solverstate"
-		$CAFFE_ROOT/build/tools/caffe train --solver=/home/shahidm/code/domain_adaptation/network/places_caffenet/places205CNN_solver.prototxt --weights=$model --log_dir=/home/shahidm/code/domain_adaptation/data/logs/ --snapshot=$snapshot -gpu all 
+		$CAFFE_ROOT/build/tools/caffe train --solver=$solver --snapshot=$snapshot --log_dir=$log_dir -gpu 0
 	else
 		echo "wrong 2nd params "
 	fi	

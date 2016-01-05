2eea4881471be7c429aa11fb58a5edd8b41eda45
Muneeb <muneebshahid5@gmail.com>
2016-01-05 13:15:04 +0100

    update preprocessing scripts

--- examples/domain_adaptation/scripts/shell/create_lmdbs_resize.sh
+++ examples/domain_adaptation/scripts/shell/create_lmdbs_resize.sh
@@ -7,15 +7,15 @@ if [ $1 = "complete" ]
 then
 	complete_lmdb=$lmdb$1
 	rm -r $complete_lmdb
-	$convert_imageset -resize_height 256 -resize_width 256 $images $images$1".txt" $complete_lmdb
+	$convert_imageset $images $images$1".txt" $complete_lmdb
 elif [ $1 = "source" ] || [ $1 = "target" ] || [ $1 = "train" ] || [ $1 = "test" ]
 then
 	lmdb1=$lmdb$1"1"
 	lmdb2=$lmdb$1"2"
 	rm -r $lmdb1
 	rm -r $lmdb2
-	$convert_imageset -resize_height 256 -resize_width 256 $images $images$1"1.txt" $lmdb1
-	$convert_imageset -resize_height 256 -resize_width 256 $images $images$1"2.txt" $lmdb2
+	$convert_imageset $images $images$1"1.txt" $lmdb1
+	$convert_imageset $images $images$1"2.txt" $lmdb2
 else
 	echo "Wrong Param"
 fi
--- examples/domain_adaptation/scripts/shell/image_resize.sh
+++ examples/domain_adaptation/scripts/shell/image_resize.sh
@@ -1,6 +1,6 @@
-image_dir=$PROJ_HOME/"data/train/"
-original_dir=$image_dir"/original_images"
-resized_dir=$image_dir"/processed_images"
+original_dir=$1
+resized_dir=$2
 for image in $original_dir/*.jpg; do
+	echo "processed $image"
 	convert $image -resize 256x256\! $resized_dir/$(basename $image)
 done	

892375578786613ab35241a4c2a9083b51c662b8
Muneeb <muneebshahid5@gmail.com>
2016-01-29 17:08:05 +0100

    new deploy, increased numopt + separate transformer

--- examples/domain_adaptation/scripts/python/calculate_scores.py
+++ examples/domain_adaptation/scripts/python/calculate_scores.py
@@ -20,22 +20,24 @@ net1 = caffe.Net(caffe_root + deploy,
 net2 = caffe.Net(caffe_root + deploy,
                  results + caffe_model, caffe.TEST)
 
-def output(net, img1, img2):
-    transformer = caffe.io.Transformer({'data_': net.blobs['data_1'].data.shape})
-    transformer.set_transpose('data_', (2, 0, 1))
-    transformer.set_mean('data_', arr[0].mean(1).mean(1))
-    transformer.set_raw_scale('data_', 255)
-    transformer.set_channel_swap('data_', (2, 1, 0))
-    img1 = transformer.preprocess('data_', caffe.io.load_image(img1))
-    img2 = transformer.preprocess('data_', caffe.io.load_image(img2))
+transformer = caffe.io.Transformer({'data_': net2.blobs['data_1'].data.shape})
+transformer.set_transpose('data_', (2, 0, 1))
+transformer.set_mean('data_', arr[0].mean(1).mean(1))
+transformer.set_raw_scale('data_', 255)
+transformer.set_channel_swap('data_', (2, 1, 0))
+
+
+def output(net, t, img1, img2):
+    img1 = t.preprocess('data_', caffe.io.load_image(img1))
+    img2 = t.preprocess('data_', caffe.io.load_image(img2))
     net.blobs['data_1'].data[...] = img1
     net.blobs['data_2'].data[...] = img2
     return net.forward()
 
 
 def print_r(img1, img2):
-    r1 = output(net1, img1, img2)
-    r2 = output(net2, img1, img2)
+    r1 = output(net1, transformer, img1, img2)
+    r2 = output(net2, transformer, img1, img2)
     print '-----------------------------------'
     print "untrained: ", r1
     print 'distance: ', np.linalg.norm(r1['fc8_n'] - r1['fc8_n_p'])

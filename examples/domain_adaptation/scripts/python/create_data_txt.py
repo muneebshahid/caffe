a00467adcea0b4de8ee61c44a60840b2bb14ac1f
Muneeb <muneebshahid5@gmail.com>
2016-02-04 15:18:53 +0100

    dismantled data loader

--- examples/domain_adaptation/scripts/python/create_data_txt.py
+++ examples/domain_adaptation/scripts/python/create_data_txt.py
@@ -3,7 +3,7 @@ import sys
 import numpy as np
 import random
 import copy
-import pandas as pd
+from data_loader import DataLoader
 
 
 def get_train_test_split_len(examples_len, split):
@@ -77,21 +77,6 @@ def get_augmented_data_pos(data_set, ext, limit=4):
                 keys_dict_2.pop(key_2)
     return augmented_data, augmented_dict
 
-
-def add_zeros_alderly(string, max_len=5):
-    len_str = len(string)
-    for i in range(max_len - len_str):
-        string = '0' + string
-    return string
-
-
-def floor_ceil_int(x):
-    remainder = x % 10
-    if remainder < 5:
-        x -= remainder
-    else:
-        x += (10 - remainder)
-    return x
 
 def get_distant_images(dataset, image_gap, gps_gap, fix_dist=False):
     data_len = len(dataset)
@@ -193,158 +178,6 @@ def create_negatives(key, dataset, length=None, augmented=False, chosen_aug=None
     return negatives
 
 
-def get_dataset(key, root_folder_path):
-    data_set = []
-    folder_path = root_folder_path + key + '/'
-    if key == 'freiburg':
-        print "processing freiburg data....."
-        with open(folder_path + 'processed_season_match.txt', "r") as data_reader:
-            data_set_freiburg = [line.replace('\n', '').split(' ') for line in data_reader.readlines()]
-
-        with open(folder_path + 'pxgps/summer_track.pxgps') as gps_reader:
-            frei_gps_data = [np.array(line.replace('\n', '').split(' ')[1:], np.float64)
-                              for line in gps_reader.readlines()]
-
-            for instance in data_set_freiburg:
-                j = 1
-                while j < len(instance):
-                    file_name, _ = osh.split_file_extension(osh.extract_name_from_path(instance[0]))
-                    gps_index = int(file_name[-7:])
-                    data_set.append([folder_path + instance[0],
-                                     folder_path + instance[j], 1, frei_gps_data[gps_index]])
-                    j += 1
-    elif key == 'michigan':
-        mich_ignore = range(1264, 1272)
-        mich_ignore.extend(range(1473, 1524))
-        mich_ignore.extend(range(1553, 1565))
-        mich_ignore.extend(range(1623, 1628))
-        # indoor
-        mich_ignore.extend(range(4795, 5521))
-        mich_ignore.extend(range(8324, 9288))
-        mich_ignore.extend(range(10095, 10677))
-        mich_ignore.extend(range(11270, 11776))
-        mich_ignore.extend(range(11985, 12575))
-        print 'Processing michigan data.....'
-        files_michigan = [folder_path + 'aug/' + im + '.jpg'
-                          for im in
-                          sorted([im[:-4] for im in
-                                  osh.list_dir(folder_path + 'aug/')], key=int)]
-
-        for im_file in files_michigan:
-            file_n = osh.extract_name_from_path(im_file)
-            if int(file_n[:-4]) in mich_ignore:
-                # print "ignoring {0}".format(file_n[:-5])
-                continue
-            data_set.append([im_file, im_file.replace('aug', 'jan'), 1, None])
-    elif key == 'fukui':
-        # mislabeled examples
-        fukui_ignore = {'SU': ['4', '04000000'],
-                        'SP': ['4', '04000000', '04000001', '04000002'],
-                        'AU': ['1', '01000000', '01000001', '01000002', '01000003'],
-                        'WI': ['None']}
-        print 'processing fukui data.....'
-        extra_im_range = 3
-        instance = ['AU', 'SP', 'SU', 'WI']
-        for season in instance:
-            print 'processing: ', season
-            season_folder = folder_path + season + '/'
-            ground_truth_folder = season_folder + 'gt/'
-            gts = sorted([gt[:-4] for gt in osh.list_dir(ground_truth_folder)], key=int)
-            print 'creating positive examples'
-            for gt in gts:
-                with open(ground_truth_folder + gt + '.txt', "r") as ground_truths:
-                    for line in ground_truths.readlines():
-                        # skip \n
-                        images = line.replace('\n', '').split(' ')
-                        qu_image = images[0]
-                        db_image = images[1]
-                        # skip mislabeled examples
-                        if gt == fukui_ignore[season][0] and qu_image in fukui_ignore[season]:
-                            print 'ignoring {0}, gt {1}, season {2}'.format(qu_image, gt, season)
-                            continue
-                        qu_image_path = get_fukui_im_path(qu_image + '.jpg', gt + '/', season_folder, True)
-                        db_image_path = get_fukui_im_path(db_image + '.jpg', gt + '/', season_folder)
-                        if not osh.is_file(qu_image_path):
-                            print "query not found", qu_image_path
-                        if not osh.is_file(db_image_path):
-                            print '---------------'
-                            print 'db not found'
-                            print qu_image_path
-                            print db_image_path
-                            print '---------------'
-                        if int(db_image) < int(images[2]):
-                            print 'db less than limit'
-                            print qu_image_path
-                            print db_image_path
-                            print '----------------'
-                        gt_example = [qu_image_path, db_image_path, 1, None]
-
-                        data_set.append(gt_example)
-                        int_db_image = int(db_image)
-                        im_range = range(int(images[2]), int(images[3]) + 1)
-
-                        # append images before if possible
-                        for im in range(int_db_image - extra_im_range, int_db_image):
-                            if im in im_range:
-                                db_image_path = get_fukui_im_path('0' + str(im) + '.jpg', gt + '/', season_folder)
-                                if osh.is_file(db_image_path):
-                                    gt_example = [qu_image_path, db_image_path, 1, None]
-                                    data_set.append(gt_example)
-
-                        # append images after if possible
-                        for im in range(int_db_image + 1, int_db_image + extra_im_range + 1):
-                            if im in im_range:
-                                db_image_path = get_fukui_im_path('0' + str(im) + '.jpg', gt + '/', season_folder)
-                                if osh.is_file(db_image_path):
-                                    gt_example = [qu_image_path, db_image_path, 1, None]
-                                    data_set.append(gt_example)
-    elif key == 'alderly':
-        frame_matches = pd.read_csv(folder_path + 'framematches.csv')
-        for value in frame_matches.values:
-            path_a = folder_path + 'FRAMESA/Image' + add_zeros_alderly(str(value[0])) + '.jpg'
-            path_b = folder_path + 'FRAMESB/Image' + add_zeros_alderly(str(value[1])) + '.jpg'
-            data_set.append([path_a, path_b, 1, None])
-    elif key == 'kitti':
-        print 'processing kitti dataset....'
-        folder_path_2k11 = folder_path + '2011_09_26/'
-        sequence_folders = osh.list_dir(folder_path_2k11)
-        images_02 = []
-        for sequence_folder in sequence_folders:
-            full_path = folder_path_2k11 + sequence_folder + '/image_02/data/'
-            images_02 = [full_path + im_file for im_file in osh.list_dir(full_path)]
-            for image_02 in images_02:
-                gps_file = image_02.replace('image_02', 'oxts').replace('.png', '.txt')
-                with open(gps_file) as gps_file_handle:
-                    gps_coordinates = np.array(gps_file_handle.readline().split(' ')[:2], np.float64)
-                data_set.append([image_02, image_02.replace('image_02', 'image_03'), 1, gps_coordinates])
-    elif key == 'nordland':
-        print 'Processing nordland data.....'
-        offset = 171
-        files_nordland = [folder_path + 'summer/' + im + '.jpg'
-                          for im in
-                          sorted([im[:-4] for im in
-                                  osh.list_dir(folder_path + 'summer/')], key=int)]
-        gps_file = pd.read_csv(folder_path + 'nordlandsbanen_time_pos.csv')
-
-        for im_file in files_nordland:
-            im_file_num, _ = osh.split_file_extension(osh.extract_name_from_path(im_file))
-            im_file_num = int(im_file_num)
-            im_file_index = floor_ceil_int(im_file_num - offset) / 10
-            summer_image = im_file
-            winter_image = im_file.replace('summer', 'winter')
-            fall_image = im_file.replace('summer', 'fall')
-            spring_image = im_file.replace('summer', 'spring')
-
-            gps_coordinates = np.array(gps_file.values[im_file_index, 1:], np.float64)
-            data_set.append([summer_image, winter_image, 1, gps_coordinates])
-            data_set.append([summer_image, spring_image, 1, gps_coordinates])
-            data_set.append([summer_image, fall_image, 1, gps_coordinates])
-            data_set.append([winter_image, spring_image, 1, gps_coordinates])
-            data_set.append([winter_image, fall_image, 1, gps_coordinates])
-            data_set.append([spring_image, fall_image, 1, gps_coordinates])
-    return data_set
-
-
 def write_data(data_set, root_folder_path, write_path, file_path, file_num=None, lmdb=True):
     file_path = write_path + '/' + file_path
     if file_num is not None:
@@ -408,7 +241,7 @@ def process_datasets(keys, root_folder_path, write_path, augmented_limit, neg_li
     test_data_pos = []
     test_data_neg = []
     for key in keys:
-        data_set_pos = get_dataset(key, root_folder_path)
+        data_set_pos = DataLoader.load(key, root_folder_path)
         # Add fukui data only for training.
         if key != 'fukui':
             train_data_pos_temp, test_data_pos_temp = split_train_test(data_set_pos)
@@ -482,15 +315,15 @@ def main():
     if not osh.is_dir(root_folder_path):
         print "source folder does'nt exist, existing....."
         sys.exit()
-    keys = ['nordland', 'freiburg', 'michigan', 'kitti', 'alderly', 'fukui']
+    keys = ['fukui', 'nordland', 'freiburg', 'michigan', 'kitti', 'alderly']
     write_path = caffe_root + '/data/domain_adaptation_data/images/'
     augmented_limit = {keys[0]: 1, keys[1]: 1, keys[2]: 1, keys[3]: 1, keys[4]: 1, keys[5]: 1}
     neg_limit = {keys[0]: 400000,
-                 keys[1]: 350000,
+                 keys[1]: 300000,
                  keys[2]: 300000,
                  keys[3]: 100000,
-                 keys[4]: 350000,
-                 keys[5]: 350000}
+                 keys[4]: 300000,
+                 keys[5]: 300000}
     process_datasets(keys, root_folder_path, write_path, None, neg_limit)
 
 if __name__ == "__main__":

867b6fa53aaa55cddba93f8f7be53b6f109e7ee5
Muneeb <muneebshahid5@gmail.com>
2016-02-04 22:22:08 +0100

    bug fixes

--- examples/domain_adaptation/scripts/python/dump_features.py
+++ examples/domain_adaptation/scripts/python/dump_features.py
@@ -15,11 +15,11 @@ def normalize(feature):
 
 def main():
     fe = FeatureExtractor(model_path, deploy_path, mean_binary_path)
-    data_1 = load_file(txt_path[0] + '/test1.txt')
-    data_2 = load_file(txt_path[1] + '/test2.txt')
+    data_1 = load_file(txt_path + '/test1.txt')
+    data_2 = load_file(txt_path + '/test2.txt')
     features = [np.empty((len(data_1), 64896)), np.empty((len(data_2), 64896))]
-    for i, im1, im2 in enumerate(zip(data_1, data_2)):
-        result = fe.extract(im1, im2)
+    for i, (im1, im2) in enumerate(zip(data_1, data_2)):
+        result = fe.extract([im1, im2], ['conv3', 'conv3_p'])
         temp_features = [result['conv3'].copy(), result['conv3_p'].copy()]
         for j, temp_feature in enumerate(temp_features):
             features[i][j] = normalize(temp_feature)

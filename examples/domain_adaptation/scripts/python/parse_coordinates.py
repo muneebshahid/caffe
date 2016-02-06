42997af8d56cc8620bcc59373924988e128b5ae4
Muneeb <muneebshahid5@gmail.com>
2016-02-06 12:52:40 +0100

    changed parse_coordiantes to accomodates simillarity

--- examples/domain_adaptation/scripts/python/parse_coordinates.py
+++ examples/domain_adaptation/scripts/python/parse_coordinates.py
@@ -3,19 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-def read_files():
-    return np.loadtxt(coord_file_txt + '0.txt'), np.loadtxt(coord_file_txt + '1.txt')
 
-
-def plot_data(qu, db):
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(qu[:, 0], qu[:, 1], qu[:, 2], c='r')
-    ax.scatter(db[:, 0], db[:, 1], db[:, 2], c='b')
-    for qu_point, db_point in zip(qu, db):
-        ax.plot([qu_point[0], db_point[0]], [qu_point[1], db_point[1]], [qu_point[2], db_point[2]], c='g')
-    plt.show()
-
+def get_sorted_arg(row, sim):
+    if sim:
+        return np.argsort(row)[::-1]
+    else:
+        return np.argsort(row)
 
 def create_score_mat(qu, db):
     score_mat = np.ones((qu.shape[0], qu.shape[0]))
@@ -27,16 +20,16 @@ def create_score_mat(qu, db):
     return np.apply_along_axis(lambda row: row / np.linalg.norm(row), 1, score_mat)
 
 
-def pr_recall(score_mat, im_range=3, threshold=1.8):
+def pr_recall(score_mat, sim=True, im_range=3, threshold=.045):
     count = 0
     true_pos, false_neg, false_pos = 0, 0, 0
     for i, row in enumerate(score_mat):
         true_pos_found = False
-        sorted_args = np.argsort(row)
+        sorted_args = get_sorted_arg(row, sim)
         closest_args = []
         for j, args in enumerate(sorted_args):
             curr_ele = score_mat[i, args]
-            if curr_ele > threshold:
+            if curr_ele < threshold:
                 closest_args = sorted_args[:j]
                 break
 
@@ -46,7 +39,7 @@ def pr_recall(score_mat, im_range=3, threshold=1.8):
                 true_pos_found = True
             else:
                 false_pos += 1
-        if not true_pos_found:
+        if true_pos_found:
             true_pos += 1
         else:
             false_neg += 1
@@ -55,10 +48,10 @@ def pr_recall(score_mat, im_range=3, threshold=1.8):
     return pr, recall
 
 
-def min_vals_around_diag(score_mat, k=5, diag=20):
+def vals_around_diag(score_mat, sim=True, k=3, diag=5):
     values_inside, values_outside = 0, 0
     for i, row in enumerate(score_mat):
-        sorted_args = np.argsort(row)[:k]
+        sorted_args = get_sorted_arg(row,sim)[:k]
         range_arr = range(i - diag, i + diag +1)
         for min_arg in sorted_args:
             if min_arg in range_arr:
@@ -68,26 +61,22 @@ def min_vals_around_diag(score_mat, k=5, diag=20):
     total_pts = values_inside + values_outside
     return values_inside / float(total_pts)
 
+
 def main():
-    #freiburg, michigan = read_files()
-    #freiburg_qu, freiburg_db = freiburg[:, :128], freiburg[:, 128:]
-    #michigan_qu, michigan_db = michigan[:, :128], michigan[:, 128:]
-    #plot_data(freiburg_qu[:100], freiburg_db[:100])score
-    #score_mat = create_score_mat(freiburg_qu, freiburg_db)
-    #score_mat = create_score_mat(michigan_qu, michigan_db)
-    #score_mat = np.loadtxt(score_txt)
-    score_file = 'scores_iter_60k.npy'
-    score_file = 'scores_untrained.npy'
-    score_mat = np.load(features_folder + 'scores_iter_60k.npy')
-    #np.savetxt(caffe_root + '/data/domain_adaptation_data/images/scores.txt', score_mat, '%10.5f')
-    print pr_recall(score_mat)
-    #print min_vals_around_diag(score_mat)
+    #freiburg_coords = np.load(coord_file_path + '0.npy')
+    score_mats = ['scores_untrained.npy', 'scores_iter_20k.npy', 'scores_iter_60k.npy']
+    for score_mat in score_mats:
+        score_mat = np.load(features_folder + score_mat)
+        #np.save(features_folder + score_file, score_mat)
+        score_mat = np.apply_along_axis(lambda row: row / np.linalg.norm(row), 1, score_mat)
+        print pr_recall(score_mat, threshold=.035)
+        #print vals_around_diag(score_mat)
     return
 
 
 if __name__ == '__main__':
     caffe_root = osh.get_env_var('CAFFE_ROOT')
-    coord_file_txt = caffe_root + '/data/domain_adaptation_data/images/coordinates'
+    coord_file_path = caffe_root + '/data/domain_adaptation_data/images/coordinates'
     score_txt = caffe_root + '/data/domain_adaptation_data/images/scores.txt'
     features_folder = caffe_root + '/data/domain_adaptation_data/features/'
     main()
--- examples/domain_adaptation/scripts/python/plot_lr_curves.py
+++ examples/domain_adaptation/scripts/python/plot_lr_curves.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 x = []
 def inv(base_lr, gamma, it, power):
     return base_lr * math.pow((1 + gamma * it), -power)
-for i in range(0, 10000):
-    x.append( inv(.01, .001, i, .75))
-plt.plot(range(0, 10000), x)
+for i in range(0, 40000):
+    x.append( inv(.000035, .0001, i, 2))
+plt.plot(range(0, 40000), x)
 plt.show()

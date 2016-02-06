00f122ebdab4f30022373af6ab236af4c0126e42
Muneeb <muneebshahid5@gmail.com>
2016-02-06 14:30:15 +0100

    added support for dumping coordinates as well

--- examples/domain_adaptation/scripts/python/dump_features.py
+++ examples/domain_adaptation/scripts/python/dump_features.py
@@ -3,13 +3,31 @@ import numpy as np
 import os_helper as osh
 
 
-def load_file(path):
-    data = []
-    with open(path) as file_handle:
-        for line in file_handle.readlines():
-            line = line.replace('\n', '').split(' ')
-            if line[1] == '1':
-                data.append(line[0])
+def normalize_matrix(matrix):
+    return np.apply_along_axis(lambda row: row / np.linalg.norm(row), 1, matrix)
+
+
+def create_score_mat(qu, db):
+    score_mat = np.ones((qu.shape[0], qu.shape[0]))
+    for i, qu_point in enumerate(qu):
+        for j, db_point in enumerate(db):
+            score_mat[i, j] = np.linalg.norm(qu_point - db_point)
+        if i % 100 == 0:
+            print i
+    return normalize_matrix(score_mat)
+
+
+def load_file(path, keys):
+    data = {}
+    for key in keys:
+        data[key] = []
+        with open(path + '/test1.txt') as test_1, open(path + '/test2.txt') as test_2:
+            for line_1, line_2 in zip(test_1.readlines(), test_2.readlines()):
+                line_1 = line_1.replace('\n', '').split(' ')
+                line_2 = line_2.replace('\n', '').split(' ')
+                if line_1[1] == '1' and line_2[1] == '1':
+                    if key in line_1[0] and key in line_2[0]:
+                        data[key].append([line_1, line_2])
     return data
 
 
@@ -19,26 +37,50 @@ def normalize(feature):
 
 
 def main():
+    keys = ['freiburg', 'michigan']
     fe = FeatureExtractor(model_path, deploy_path, mean_binary_path)
-    data_1 = load_file(txt_path + '/test1.txt')
-    data_2 = load_file(txt_path + '/test2.txt')
-    features_1, features_2 = [], []
-    for i, (im1, im2) in enumerate(zip(data_1, data_2)):
-        #result = {'conv3': np.random.rand(64896), 'conv3_p': np.random.rand(64896)}#fe.extract([im1, im2], ['conv3', 'conv3_p'])
-        result = fe.extract([im1, im2], ['conv3', 'conv3_p'])
-        features_1.append(normalize(result['conv3'].copy()))
-        features_2.append(normalize(result['conv3_p'].copy()))
-        if i % 100 == 0:
-            print '{0} / {1}'.format(i, len(data_1))
-    print 'writing files....'
-    np.save(save_path + 'features_1', np.array(features_1))
-    np.save(save_path + 'features_2', np.array(features_2))
+    data = load_file(txt_path, keys)
+    for key in data:
+        print 'processing: ', key
+        coordinates_1, coordinates_2 = [], []
+        features_1, features_2 = [], []
+        key_data = data[key]
+        for i, image_pair in enumerate(key_data):
+            im1, im2 = image_pair[0], image_pair[1]
+            result = fe.extract([im1, im2], ['conv3', 'conv3_p'])
+            features_1.append(normalize(result['conv3'].copy()))
+            features_2.append(normalize(result['conv3_p'].copy()))
+            coordinates_1.append(result['fc8_n'][0].copy())
+            coordinates_2.append(result['fc8_n_p'][0].copy())
+            if i % 500 == 0:
+                print '{0} / {1}'.format(i, len(key_data))
 
+        print 'converting features to nd arrays...'
+        features_1 = np.array(features_1)
+        features_2 = np.array(features_2)
+        print 'calculating cos similarity...'
+        score_mat = features_1.dot(features_2.T)
+        score_mat = normalize_matrix(score_mat)
+        print 'writing files...'
+        np.save(save_path + '_' + key + '_features_1', np.array(features_1))
+        np.save(save_path + '_' + key + '_features_2', np.array(features_2))
+        np.save(save_path + '_' + key + '_cos_sim', np.array(score_mat))
+
+        print 'converting coordinates to nd arrays...'
+        coordinates_1 = np.array(coordinates_1)
+        coordinates_2 = np.array(coordinates_2)
+        print 'calculating score mat...'
+        score_mat = create_score_mat(coordinates_1, coordinates_2)
+        print 'writing files...'
+        np.save(save_path + '_' + key + '_coordinates_1', np.array(coordinates_1))
+        np.save(save_path + '_' + key + '_coordinates_2', np.array(coordinates_2))
+        np.save(save_path + '_' + key + '_euc_dist', np.array(score_mat))
+    print 'done'
 
 if __name__ == '__main__':
     caffe_root = osh.get_env_var('CAFFE_ROOT')
     txt_path = caffe_root + '/data/domain_adaptation_data/images/'
-    save_path = caffe_root + '/data/domain_adaptation_data/features/'
+    save_path = caffe_root + '/data/domain_adaptation_data/results/'
     deploy_path = caffe_root + '/examples/domain_adaptation/network/deploy.prototxt'
     model_path = caffe_root + '../results/curr.caffemodel'
     mean_binary_path = caffe_root + '../data/models/alexnet/pretrained/places205CNN_mean.binaryproto'

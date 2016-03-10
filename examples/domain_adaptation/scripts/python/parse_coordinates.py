6023ce6fff5d87ce7b07e9518a83f3ecf3cddb38
Muneeb <muneebshahid5@gmail.com>
2016-03-10 19:15:17 +0100

    modified

--- examples/domain_adaptation/scripts/python/parse_coordinates.py
+++ examples/domain_adaptation/scripts/python/parse_coordinates.py
@@ -32,7 +32,7 @@ def pr_recall(score_mat, sim=True, im_range=3, threshold=.045):
             if sim and curr_ele < threshold:
                 closest_args = sorted_args[:j]
                 break
-            elif not sim and curr_ele >= threshold:
+            elif not sim and curr_ele > threshold:
                 closest_args = sorted_args[:j]
                 break
 
@@ -54,7 +54,7 @@ def pr_recall(score_mat, sim=True, im_range=3, threshold=.045):
     return pr, recall
 
 
-def vals_around_diag(score_mat, sim=True, k=3, diag=5):
+def vals_around_diag(score_mat, sim=True, k=3, diag=3):
     values_inside, values_outside = 0, 0
     for i, row in enumerate(score_mat):
         sorted_args = get_sorted_arg(row,sim)[:k]
@@ -69,53 +69,53 @@ def vals_around_diag(score_mat, sim=True, k=3, diag=5):
 
 
 def main():
-    score_data = [#['untrained_places205CNN_iter_300000_upgraded.caffemodel_freiburg_cos_sim.npy',
-                   #0.0443317064121, 0.0274826074699, True]]
+    score_data = [
                    #['untrained_places205CNN_iter_300000_upgraded.caffemodel_nordland_fc7fc7_p_cos_sim.npy',
                    #0.03, 0.011, True]]
-                   #['untrained_places205CNN_iter_300000_upgraded.caffemodel_nordland_cos_sim.npy',
-                   #0.0225, 0.012656565656565, True]]
+                   #['untrained_places205CNN_iter_300000_upgraded.caffemodel_nordland_cos_summer_conv3_norm_winter_conv3_norm.npy',
+                   #0.0225, 0.0105, True]]
+                   #['untrained_places205CNN_iter_300000_upgraded.caffemodel_nordland_cos_summer_conv3_norm_spring_conv3_norm.npy',
+                   #0.0233060606061, 0.0096, True]]
+                   #['untrained_places205CNN_iter_300000_upgraded.caffemodel_nordland_cos_summer_conv3_norm_fall_conv3_norm.npy',
+                   #0.0314, 0.012, True]]
+		   #['untrained_places205CNN_iter_300000_upgraded.caffemodel_nordland_euc_conv3_conv3_p.npy',
+                   #30.006, 0.00966666666667, False]]
 		   #['nordland_only_snapshots_iter_140000.caffemodel_nordland_cos_sim.npy',
                    #0.0159909090909, 0.0115, True]]
-                   #['nordland_only_snapshots_iter_140000.caffemodel_freiburg_cos_sim.npy',
-                   #0.0370626383018, 0.0266, True]] 
-		   #['nordland_only_snapshots_iter_120000_10_margin.caffemodel_nordland_cos_conv3_norm_conv3_p_norm.npy',
-                   #0.0241515151515, .013, True]] 
+                   #['nordland_only_snapshots_iter_140000.caffemodel_nordland_euc_conv3_conv3_p.npy',
+                   #0.006, 0.0095, False]] 
+		   #['nordland_only_snapshots_iter_60000.caffemodel_nordland_euc_summer_conv3_winter_conv3.npy',
+                   #0.0063, .01046, False]]
+                   #['nordland_only_snapshots_iter_60000.caffemodel_nordland_euc_summer_conv3_fall_conv3.npy',
+                   #0.00375100671141, .01, False]]
 		   #['nordland_only_snapshots_iter_120000_10_margin.caffemodel_nordland_euc_fc8_n_fc8_n_p.npy',
                    #.06, 1.5, False]]
-		   ['nordland_only_snapshots_iter_120000_10_margin.caffemodel_nordland_euc_fc7_fc7_p.npy',
-                   .00075, .0057, False]] 
-		   #['nordland_only_snapshots_iter_120000_10_margin.caffemodel_nordland_euc_conv3_conv3_p.npy',
-                   #.0073, .0095, False]]
-		   #['nordland_only_snapshots_iter_15000_mich.caffemodel_michigan_cos_sim.npy',
-                   #0.0303479569973, 0.0214891774892, True]]
-                   #['triplet_loss_snapshots_iter_120000.caffemodel_freiburg_cos_sim.npy',
-                   #0.0376727272727, 0.0268236914601, True]]
-		   #['triplet_loss_snapshots_iter_120000.caffemodel_nordland_cos_sim.npy',
-                   #0.018585858585858585, 0.0113, True]]
-		   #['triplet_loss_snapshots_iter_40000.caffemodel_freiburg_cos_sim.npy',
-                   #0.045, 0.0271908896124, True]]
-		   #['triplet_loss_snapshots_iter_40000.caffemodel_nordland_cos_sim.npy',
-                   #0.0209595959596, 0.0120931537598, True]]
-		   #['triplet_loss_snapshots_iter_40000.caffemodel_michigan_cos_sim.npy',
-                   # 0.0369, 0.0226172703466, True]]
-		   #['triplet_loss_four_snapshots_iter_40000.caffemodel_freiburg_cos_sim.npy',
-                   #0.045, 0.0271908896124, True]]
-		   #['triplet_loss_four_snapshots_iter_40000.caffemodel_michigan_cos_sim.npy',
-                   #0.0382323232323, 0.0229466380981, True]]
-		   #['triplet_loss_four_snapshots_iter_33000_10_margin.caffemodel_nordland_cos_sim.npy',
-                   #.013, 0.0106, True]]
-		   #['triplet_loss_four_snapshots_iter_40000.caffemodel_freiburg_euc_dist.npy',
-                   #70, 110, False]]
-		   #['triplet_loss_snapshots_iter_120000.caffemodel_michigan_euc_dist.npy', 0, .1, False]]
-                   #['mix_data_snapshots_iter_200000.caffemodel_michigan_cos_sim.npy', 0.03, 0.0213131313131, True]]
+		   #['nordland_only_snapshots_iter_120000_10_margin.caffemodel_nordland_euc_fc7_fc7_p.npy',
+                   #.00075, .0057, False]] 
+		   #['nordland_only_snapshots_iter_120000.caffemodel_nordland_euc_summer_conv3_winter_conv3.npy',
+                   #0.00638832214765, .01039, False]]
+		   #['nordland_only_snapshots_iter_120000.caffemodel_nordland_euc_summer_conv3_spring_conv3.npy',
+                   #.00452, .0108, False]]
+                   #['nordland_only_snapshots_iter_120000.caffemodel_nordland_euc_summer_conv3_fall_conv3.npy',
+                   #.004, .0087, False]]
+                   #['untrained_places205CNN_iter_300000_upgraded.caffemodel_freiburg_cos_summer_conv3_norm_winter_conv3_norm.npy',
+                   #0.0485503355705, 0.0248862663844, True]]
+                   #['freiburg_only_snapshots_iter_5000.caffemodel_freiburg_euc_summer_conv3_winter_conv3.npy',
+                   #.0202, .0275, False]]
+		   ['freiburg_only_snapshots_iter_120000.caffemodel_freiburg_euc_summer_conv3_winter_conv3.npy',
+                   .0202, .0275, False]]
+		   #['final.npy',
+                   #.005, .0108, False]]
     for data in score_data:
         print 'processing: {0}'.format(data[0])
         score_mat = np.load(results_folder + data[0])
         pr_recal_list = []
         score_mat = np.apply_along_axis(lambda row: row / np.linalg.norm(row), 1, score_mat)
+        #score_mat = np.apply_along_axis(lambda row: row / np.max(row), 1, score_mat)
+        #score_mat = np.divide(np.subtract(score_mat, np.min(score_mat)), np.max(score_mat) - np.min(score_mat))
 	print score_mat
-        values = np.linspace(data[1], data[2], 100)
+        #print vals_around_diag(score_mat, data[-1])
+        values = np.linspace(data[1], data[2], 150)
         for i, value in enumerate(values):
             pr_recal_result = pr_recall(score_mat, threshold=value, sim=data[-1])
             pr_recal_list.append(pr_recal_result)
@@ -128,5 +128,5 @@ if __name__ == '__main__':
     caffe_root = osh.get_env_var('CAFFE_ROOT')
     coord_file_path = caffe_root + '/data/domain_adaptation_data/images/coordinates'
     score_txt = caffe_root + '/data/domain_adaptation_data/images/scores.txt'
-    results_folder = caffe_root + '/data/domain_adaptation_data/results/'
+    results_folder = caffe_root + '/data/domain_adaptation_data/results/freiburg/'
     main()
